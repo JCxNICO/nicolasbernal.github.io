32f20a61a26c41e84d745a8e4833fa2b8faf58ab
JCxNICO <bernalmontoya2000@gmail.com>
2024-09-25 18:45:47 -0500

    Register
    Login and Register

---
+++ php/conexion_be.php
@@ -0,0 +1,12 @@
+<?php
+
+
+    $conexion = mysqli_connect("localhost", "root", "", "login_register_db");
+
+   /* if($conexion){
+        echo 'Conectado exitosamente a la Basa de Datos';
+    }else{
+        echo 'no se pudo conentar a la bd';
+    } */
+
+?>
